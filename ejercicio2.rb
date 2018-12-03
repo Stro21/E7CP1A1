9650cc697aee3b20b84e641eee50a09729490d9f
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 19:39:14 -0300

    Se empezó la segunda parte del ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,4 +1,8 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
 
+# Parte 1
 productos.each { |producto, valor| puts producto }
-#Se termino la parte 1.
+
+# Parte 2
+
+producto[2200] = cereal
