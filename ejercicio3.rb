57c28461bcfd1e6bb989da99624889cc0f62f56a
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 20:28:27 -0300

    Se completó la parte 3 del ejercicio 3.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -3,11 +3,20 @@ h = {"x": 1, "y":2}
 # 1. Agregar el string z con el valor 3.
 
 h["z"] = 3
-puts h["z"]
+h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+puts ''
 
 # 2. Cambiar el valor de x por 5.
 
+h[:"x"] = 5
+h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+puts ''
+
 # 3. Eliminar la clave y.
+
+h.delete(:"y")
+h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+puts ''
 
 # 4. Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
 
