fb34523d5cb10177c92f0436eed3b449faf5036b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 20:15:24 -0300

    Empezando el ejercicio 3.

---
+++ ejercicio3.rb
@@ -0,0 +1,11 @@
+h = {"x": 1, "y":2}
+
+# 1. Agregar el string z con el valor 3.
+
+# 2. Cambiar el valor de x por 5.
+
+# 3. Eliminar la clave y.
+
+# 4. Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
+
+# 5. Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
