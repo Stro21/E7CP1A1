14b09c823a6fd1537aa2e6eb966402fc5ed1dcea
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 20:31:49 -0300

    Se completo la parte 4 del ejercicio 3.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,23 +1,22 @@
 h = {"x": 1, "y":2}
 
 # 1. Agregar el string z con el valor 3.
-
 h["z"] = 3
 h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
 puts ''
 
 # 2. Cambiar el valor de x por 5.
-
 h[:"x"] = 5
 h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
 puts ''
 
 # 3. Eliminar la clave y.
-
 h.delete(:"y")
 h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
 puts ''
 
 # 4. Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
+h.each {|variable, valor| puts "yeeah" if variable == "z"}
+puts ''
 
 # 5. Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
