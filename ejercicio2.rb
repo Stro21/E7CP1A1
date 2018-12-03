cfccf726a0a97c65b56c5fd6522a3aecb989a485
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 20:00:15 -0300

    Se completo la cuarta parte del ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,14 +1,20 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
 
-# Parte 1 Imprimir todos los productos.
+# 1. Imprimir todos los productos.
 productos.each { |producto, valor| puts producto }
 puts ''
 
-# Parte 2 Agregar un producto nuevo llamado cereal.
+# 2. Agregar un producto nuevo llamado cereal.
 productos['cereal'] = 2200
 productos.each { |producto, valor| puts 'El producto ' + producto + ' cuesta ' + valor.to_s}
 puts ''
 
-# Parte 3 Cambiar el valor del producto bebida.
+# 3. Cambiar el valor del producto bebida.
 productos['bebida'] = 2000
 productos.each { |producto, valor| puts 'El producto ' + producto + ' cuesta ' + valor.to_s}
+puts ''
+
+# 4. Convertir el hash en un array y guardarlo en una nueva variable.
+productos1 = productos.to_a
+print productos1
+puts ''
