63b5f043262140e94c81394f22d4eaab83a310a1
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 19:49:43 -0300

    Se completo la tercera parte del ejercicio 2.

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,8 +1,14 @@
 productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
 
-# Parte 1
+# Parte 1 Imprimir todos los productos.
 productos.each { |producto, valor| puts producto }
+puts ''
 
-# Parte 2
+# Parte 2 Agregar un producto nuevo llamado cereal.
 productos['cereal'] = 2200
+productos.each { |producto, valor| puts 'El producto ' + producto + ' cuesta ' + valor.to_s}
+puts ''
+
+# Parte 3 Cambiar el valor del producto bebida.
+productos['bebida'] = 2000
 productos.each { |producto, valor| puts 'El producto ' + producto + ' cuesta ' + valor.to_s}
