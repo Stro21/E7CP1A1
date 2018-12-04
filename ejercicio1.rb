7b049421793d28ac91e4e0954bef3501ecd5dce9
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-04 16:27:05 -0300

    Se arreglo.

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-h = { claveuno: 10, clavedos: 20, clavetres: 30 }
-=======
 h = { claveuno: 10, clavedos: 20, clavetres: 30 }
 h.each { |llave, valor| puts llave + ' ' + valor }
->>>>>>> developer
