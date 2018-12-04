fb05dbd571ddce9b36c08287c9f3c717ec192d8b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 21:21:00 -0300

    Se empezó el ejercicio 4.

---
+++ ejercicio4.rb
@@ -0,0 +1,8 @@
+personas = ["Carolina", "Alejandro", "Maria Jesús", "Valentín"]
+edades = [32, 28, 41, 19]
+
+# 1. Se pide generar un hash con la información:
+personas_hash = {"Carolina": 32, "Alejandro":28, "María Jesús":41, "Valentín":19}
+
+
+# 2. Crear un método que reciba el hash y devuelva el promedio de las edades del hash pasado como argumento.
