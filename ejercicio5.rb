a55c68a936470362281dde6d90833e98fd9fc03b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-03 21:53:14 -0300

    Se avanza en el ejercicio 5.

--- ejercicio5.rb
+++ ejercicio5.rb
@@ -1,7 +1,7 @@
 meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
 ventas = [2000, 3000, 1000, 5000, 4000]
 
-# Generar un hash que contenga los meses como llave y las ventas como valor:
+# 1. Generar un hash que contenga los meses como llave y las ventas como valor:
 # h = {'Enero': 2000, 'Febrero': 3000 ... }
 
 def generar_hash(arr1, arr2)
@@ -16,3 +16,10 @@ end
 
 hash = generar_hash(meses, ventas)
 hash.each {|mes, venta| puts 'El mes es ' + mes.to_s + ' y su ventas son ' + venta.to_s}
+
+# 2. En base al hash generado:
+# 2.1 Invertir las llaves y los valores del hash.
+
+
+
+# 2.2 Obtener el mes mayor cantidad de ventas (a partir del hash invertido.)
