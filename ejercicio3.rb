7b71bf3dda1cc198708ccefd067d014e39ed882b
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-05 09:30:53 -0300

    Se hicieron unos cambios para que eliminar los warnings y infos de rubocop.

--- ejercicio3.rb
+++ ejercicio3.rb
@@ -1,25 +1,29 @@
-h = {"x": 1, "y":2}
+# rubocop:disable LineLength
+
+h = { "x": 1, "y": 2 }
 
 # 1. Agregar el string z con el valor 3.
-h["z"] = 3
-h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+h['z'] = 3
+h.each { |variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s }
 puts ''
 
 # 2. Cambiar el valor de x por 5.
-h[:"x"] = 5
-h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+h[:x] = 5
+h.each { |variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s }
 puts ''
 
 # 3. Eliminar la clave y.
-h.delete(:"y")
-h.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+h.delete(:y)
+h.each { |variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s }
 puts ''
 
 # 4. Si el hash tiene una clave llamada z mostrar en pantalla "yeeah".
-h.each {|variable, valor| puts "yeeah" if variable == "z"}
+h.each { |variable, _valor| puts 'yeeah' if variable == 'z' }
 puts ''
 
 # 5. Invertir el diccionario de forma que los valores sean las llaves y las llaves los valores
 
 invertido = h.invert
-invertido.each {|variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s}
+invertido.each { |variable, valor| puts 'valor de variable ' + variable.to_s + ' es ' + valor.to_s }
+
+# rubocop:enable LineLength
--- ejercicio5.rb
+++ ejercicio5.rb
@@ -1,4 +1,6 @@
-meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+# rubocop:disable LineLength
+
+meses = %w[Enero Febrero Marzo Abril Mayo]
 ventas = [2000, 3000, 1000, 5000, 4000]
 
 # 1. Generar un hash que contenga los meses como llave y las ventas como valor:
@@ -15,15 +17,17 @@ def generar_hash(arr1, arr2)
 end
 
 hash = generar_hash(meses, ventas)
-hash.each {|mes, venta| puts 'El mes es ' + mes.to_s + ' y su ventas son ' + venta.to_s}
+hash.each { |mes, venta| puts 'El mes es ' + mes.to_s + ' y su ventas son ' + venta.to_s }
 puts ''
 
 # 2. En base al hash generado:
 # 2.1 Invertir las llaves y los valores del hash.
 
 invertido = hash.invert
-invertido.each {|venta, mes| puts venta.to_s + ' fueron las ventas del mes ' + mes.to_s}
+invertido.each { |venta, mes| puts venta.to_s + ' fueron las ventas del mes ' + mes.to_s }
 puts ''
 # 2.2 Obtener el mes mayor cantidad de ventas (a partir del hash invertido.)
 
-puts invertido.max_by{|k,v| k}
+puts invertido.max_by { |k,_v| k }
+
+# rubocop:enable LineLength
