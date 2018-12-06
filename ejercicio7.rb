5d74f2e33d571c41809b3eaac375390e9af0bcf6
Eduardo Straub <eduardo.straub1@gmail.com>
2018-12-05 21:52:13 -0300

    Se empieza el ejercicio 7.

---
+++ ejercicio7.rb
@@ -0,0 +1,17 @@
+# rubocop:disable LineLength
+# rubocop:disable AsciiComments
+
+inventario = { "Notebooks": 4, "PC Escritorio": 6, "Routers": 10, "Impresoras": 6 }
+
+# Crear un menú de 4 opciones, es decir, el usuario puede ingresar 1 2 3 o 4 y según eso el programa realizará distintas funciones.
+# Si el usuario ingresa 1, podrá agregar un item y su stock en un solo string y agregarlo al hash. Para separar el nombre del stock el usuario debe utilizar una coma.
+#   Ejemplo del input: "Pendrives, 100"
+# Si el usuario ingresa 2, podrá eliminar un item.
+# Si el usuario ingresa 3, puede actualizar la información almacenada (item y stock).
+# Si el usuario ingresa 4, podrá ver el stock total (suma del stock de cada item) que hay en el negocio.
+# Si el usuario ingresa 5, podrá ver el ítem que tiene la mayor cantidad de stock.
+# Si el usuario ingresa 6 podrá ingresar y preguntarle al sistema si un item existe en el inventario o no. Por ejemplo, el usuario ingresará "Notebooks" y el programa responderá "Sí".
+# El programa debe repertirse hasta que el usuario ingrese 7 (salir).
+
+# rubocop:enable LineLength
+# rubocop:enable AsciiComments
